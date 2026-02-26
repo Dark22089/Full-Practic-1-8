7e3dffdec9d8d7552ab3d178ea22fc2c85c2a702
Dark22089 <tem.2008msk@gmail.com>
2026-02-26 14:11:32 +0300

    Create num1.py

---
+++ PR8/num1.py
@@ -0,0 +1,21 @@
+import re
+
+pattern = r'^\d{12}$'
+
+while True:
+  try:
+    inn = int(input())
+  except ValueError:
+    print("Вы ввели неверный тип данных.")
+  except Exception as e:
+    print("F")
+  else:
+    innStr = str(inn)
+    if len(innStr) == 12:
+
+      print("ИНН принят: ", inn)
+      break
+    else:
+      print("Необходимо 12 чисел")
+  finally:
+    pass
