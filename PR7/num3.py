855cd48e9b404c9807950babeba2c4505f4153f3
Dark22089 <tem.2008msk@gmail.com>
2026-02-26 12:25:45 +0300

    Create num3.py

---
+++ PR7/num3.py
@@ -0,0 +1,15 @@
+import re
+
+text = """
+Посетите наши страницы:
+Email: info@example.com (главный), support-team@corp.net (поддержка)
+Дата: 25-05-2023, 01/01/2024, 12.02.2025.
+Коды товаров: ID_PROD-1001, ITEM-20-A, ID-300-B.
+Суммы: $150.50, 20 EUR, 500 RUB, £75.
+"""
+spec = [r"\.", r"\-", r"\/"]
+p = []
+for i in range(0, 3):
+    pattern = rf'\b\d{{2}}{spec[i]}\d{{2}}{spec[i]}\d{{4}}\b'
+    p.extend(re.findall(pattern, text))	
+print(p)
