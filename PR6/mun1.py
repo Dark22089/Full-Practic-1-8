165bd2ec9f97cb28a0bb8a78b905201cad4d7295
Dark22089 <tem.2008msk@gmail.com>
2026-02-27 11:59:07 +0300

    Update mun1.py

--- PR6/mun1.py
+++ PR6/mun1.py
@@ -1,67 +1,61 @@
-class cafe:   # Доделать
+class cafe:
     def __init__(self):
         self.menuPos = {}
         self.ordersPos = {}
+
     def add_position(self, pos, prise, category, time):
         self.menuPos[pos] = {
             "Цена": prise,
             "Категория": category,
             "Время приготовления": time}
+
     def addOrder(self, table, status):
-        priseForOrder = 0
+        priceForOrder = 0
         userOrder = []
-        print(self.menuPos.keys())
+        print(f"Доступное меню: {list(self.menuPos.keys())}")
+        
         while True:
             pos = input("Введите название блюда: ")
             if pos in self.menuPos:
-              userOrder.append(pos)
-              
-              kolvo = int(input("Введите кол-во товара: "))
-
-              priseForOrder += (self.menuPos[pos]["Цена"] * kolvo)
-              self.ordersPos[table] = {
-                  "заказы": userOrder,
-                  "статус": status
-              }
-
-              continueOrder = int(input("Продолжить заказ? (1 - да; 0 - нет)"))
-              while continueOrder != 0 and continueOrder != 1:
-                  continueOrder = int(input("Продолжить заказ? (1 - да; 0 - нет)"))
-              
-              if continueOrder == 0:
-                  break
+                kolvo = int(input("Введите кол-во товара: "))
+                for _ in range(kolvo):
+                    userOrder.append(pos)
+                
+                priceForOrder += (self.menuPos[pos]["Цена"] * kolvo)
+                
+                continueOrder = input("Продолжить заказ? (1 - да; 0 - нет): ")
+                if continueOrder == "0":
+                    break
             else:
-              print("Такого блюда нет в меню")
-        return priseForOrder
+                print("Такого блюда нет в меню")
+        self.ordersPos[table] = {
+            "заказы": userOrder,
+            "статус": status
+        }
+        return f"Итого к оплате: {priceForOrder} руб."   
+
     def Popular(self):
       localOrder = []
       
       for info in self.ordersPos.values():
             localOrder.extend(info["заказы"])
-
       if not localOrder:
           return "Заказов еще не было."
 
-      print(localOrder)
     def category(self):
-       Prod = {}     
-       for info in self.ordersPos.values():
-            for newKey in info["заказы"]:
-                category = self.menuPos[newKey]["Категория"]
-                price = self.menuPos[newKey]["Цена"]
-                Prod[cat] = Prod.get(cat, 0) + price
-                
-                if category in Prod:
-                    Prod[category] += price
-                else:
-                    Prod[category] = price
-       if not Prod:
-            print("Продаж пока не было.")
-       else:
-            for cat, total in Prod.items():
-                print(f"{cat}: {total} руб.")
+        prod = {}     
+        for info in self.ordersPos.values():
+            for dish in info["заказы"]:
+                category = self.menuPos[dish]["Категория"]
+                price = self.menuPos[dish]["Цена"]
+                prod[category] = prod.get(category, 0) + price
         
-       return Prod
+        if not prod:
+            print("Продаж пока не было.")
+        else:
+            for cat, total in prod.items():
+                print(f"{cat}: {total} руб.")
+        return prod
 
 myCafe = cafe()
 
@@ -72,3 +66,4 @@ myCafe.add_position("Чизкейк", 300, "Десерты", "2 мин")
 print(myCafe.addOrder(2, "В очереди"))
 
 print(myCafe.Popular())
+myCafe.category()
